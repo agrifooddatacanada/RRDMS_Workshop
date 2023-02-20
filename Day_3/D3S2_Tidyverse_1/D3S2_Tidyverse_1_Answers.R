76232aedf1622c9d3d377033d81be839b2f60211
Lucas Alcantara <73548101+lucas-alcantara@users.noreply.github.com>
2023-02-20 16:07:06 -0500

    Update D3S2

--- Day_3/D3S2_Tidyverse_1/D3S2_Tidyverse_1_Answers.R
+++ Day_3/D3S2_Tidyverse_1/D3S2_Tidyverse_1_Answers.R
@@ -39,13 +39,14 @@ filter(env_data, barn == "lactating")
 
 ## Your turn ----
 ### From inside the barn ----
-
+filter(env_data, location == "inside")
 ### Above 30C ----
-
+filter(env_data, temp > 30)
 ### Between 0 and 10C inside the replacement barn ----
-
+filter(env_data, temp >= 0, temp <= 10, location == "inside",
+       barn == "sp_needs")
 ### From June 3, 2022, or June 4, 2022 ----
-
+filter(env_data, date == "2022-06-04" | date == "2022-06-05")
 
 # select() ----
 ## Example ----
@@ -62,6 +63,9 @@ select(env_data, ends_with("e"))
 
 ## Your turn ----
 ### Relative humidity from inside the Special Needs barn ----
+sp_needs <- filter(env_data, barn == "sp_needs", location == "inside")
+sp_needs <- select(sp_needs, date, time, rh)
+sp_needs
 
 # arrange() ----
 ## Example ----
@@ -90,6 +94,10 @@ env_data %>% filter(barn == "sp_needs", location == "inside")
 # Use filter(), select(), arrange() and the pipe operator %>% to show only 
 # relative humidity from inside the Special Needs barn, 
 # ordered ascending by relative humidity
+env_data %>% 
+  filter(barn == "sp_needs", location == "inside") %>%
+  select(date, time, rh) %>%
+  arrange(rh)
 
 # summarise() ----
 ## Example ----
@@ -104,6 +112,15 @@ env_data %>%
             max_temp = max(temp, na.rm = TRUE))
 
 ## Your turn ----
+env_data %>% 
+  filter(barn == "sp_needs",
+         location == "inside") %>%
+  summarize(min_temp = min(temp, na.rm = TRUE),
+            avg_temp = mean(temp, na.rm = TRUE),
+            max_temp = max(temp, na.rm = TRUE),
+            min_rh = min(rh, na.rm = TRUE),
+            avg_rh = mean(rh, na.rm = TRUE),
+            max_rh = max(rh, na.rm = TRUE))
 
 # group_by ----
 ## Example ----
@@ -141,6 +158,14 @@ sampled_env_data %>%
             max_temp = max(temp, na.rm = TRUE))
 
 ## Your turn ----
+env_data %>% 
+  filter(location == "inside") %>% 
+  na.omit() %>% 
+  group_by(barn, location) %>% 
+  summarize(min_temp = min(temp),
+            max_temp = max(temp),
+            min_rh = min(rh),
+            max_rh = max(rh))
 
 # mutate() ----
 ## Example ----
@@ -154,3 +179,15 @@ env_data %>%
 # Using the functions from today, create the following table to answer 
 # our ultimate question: Are the temperatures inside the barns 
 # milder than outside?
+env_data %>% 
+  na.omit() %>% 
+  mutate(season = if_else(date >= "2021-12-31" & date <= "2022-03-20",
+                          true = "winter", 
+                          false = if_else(date >= "2022-06-21" & 
+                                            date <= "2022-09-23",
+                                          true = "summer",
+                                          false = "spring/fall"))) %>%
+  filter(season %in% c("summer", "winter")) %>% 
+  group_by(barn, season, location) %>%
+  summarise(avg_temp = mean(temp)) %>%
+  arrange(desc(barn), desc(season))
